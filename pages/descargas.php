0969444a58221a638f18f986fa83d020eec44e1c
Federico <fdefortuny@gmail.com>
2023-07-28 23:52:19 -0300

    refactor: cambio de componente profiles a options con boton menu

--- pages/descargas.php
+++ pages/descargas.php
@@ -13,10 +13,10 @@
 
 <body>
     <div>
-        <div class="link-profiles-div">
+        <div class="link-options-div">
             <?php
-            require "./components/profiles.php";
-            echo $profiles;
+            require "./components/options.php";
+            echo $options;
             ?>
         </div>
         <h1>DESCARGAS</h1>
