3090a66858d097e658a8029aab6313aae07fd539
[Damian Despan] <[damiandespan@gmail.com]>
2023-08-24 21:02:20 -0300

    arregle el scrol y agregue titulos en las etiquetas a del menu hamburguer

--- public/js/functions/nav-and-footer.js
+++ public/js/functions/nav-and-footer.js
@@ -6,7 +6,7 @@ function loadNav() {
         <input class="form-control me-2" type="search" placeholder="Buscar en Digitalmarket" aria-label="Search"/>
         <button class="btn btn-outline-success" type="submit">Buscar</button>
       </form>
-      <a href="/pages/carrito.html" class="carrito">
+      <a href="./pages/carrito.html" class="carrito">
         <img class="carrito" src="./assets/Carts_Icons.png" alt="" />
       </a>
     </nav>
@@ -14,7 +14,7 @@ function loadNav() {
     
     
       <ul>
-        <li><a href="#">Inicio</a></li>
+        <li><a href="./index.html">Inicio</a></li>
         <li><a href="#">Acerca de</a></li>
         <li><a href="#">Servicios</a></li>
         <li><a href="#">Contacto</a></li>
@@ -28,30 +28,30 @@ function loadNav() {
         </button>
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Digital</h5>
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">CATEGORIAS</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#">Almacen</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Desayuno, merienda y postres</a>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#">Aguas y Bebidas</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Frutas y verduyras</a>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Lacteos y quesos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Otras cosas</a>
+          </li>
+          </li>
+              
             </ul>
             <form class="d-flex mt-3" role="search">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -80,9 +80,9 @@ function loadNav() {
            <p>Coso pere 12346</p>
         </div>
         <div class="footer-icons">
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.whatsapp.com/"><i class="bi bi-whatsapp"></i></a>
+          <a href="#"><i class="bi bi-messenger"></i></a>
         </div>
         <p id="copy">&copy; 2023 Bytebusters. Todos los derechos reservados.</p>
       </div>
